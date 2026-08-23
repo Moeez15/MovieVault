07d1308cd8e22dde13b1b41b5e5f3322204e2c74
Moeez Ahmad <moeez0915@gmail.com>
2026-08-23 09:35:54 -0400

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; top-margin: 50px">Inventra API</h1>');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
